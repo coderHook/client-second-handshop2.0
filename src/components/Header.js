713e107194068305928881cfc46eba9798d96d32
coderhook <abel1987@hotmail.es>
2019-07-04 22:43:49 +0200

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,14 +16,14 @@ export default class Header extends Component {
           <h3>Sell the stuff that you dont use anymore and give it a second Chance to shine.</h3>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
         <br />
-        <Link to="/">
+        
           <button>
             { url[url.length - 1] === 'publishAd' 
               ? 'Buy Stuff'
               : 'Publish Add'
             }
           </button>
-        </Link>
+        
           </div>
       </header>
 

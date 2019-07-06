36d76a18d88d6005146297a56f05d9a3206f12c6
coderhook <abel1987@hotmail.es>
2019-07-06 23:02:42 +0200

    Add menu header with material

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import Home from './components/Home'
 import SingleAD from './components/SingleAD'
 import NewAdvertisement from './components/NewAdvertisement'
 import Header from './components/Header'
+import Menu from './components/Menu'
+
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Header />
+        <Menu />
+        {/* <Header /> */}
         <div>
           <Route path="/" exact component={Home} />
           <Route path="/advertisement/:id" component={SingleAD} />

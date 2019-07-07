fea2dcf7f6ae330e24ad4af6402f83952b6421f7
coderhook <abel1987@hotmail.es>
2019-07-07 11:35:16 +0200

    Add controlled Form

--- src/components/NewAdvertisement.js
+++ src/components/NewAdvertisement.js
@@ -18,63 +18,82 @@ export default class NewAdvertisement extends Component {
 
     const newAd = this.state 
 
-    // this.setState({
-    //   title: "",
-    //   picture: "",
-    //   description: "",
-    //   price: "",
-    //   email: "",
-    //   phone: "",
-    // })
-
-    console.log('before posting', newAd)
+    this.setState({
+      title: "",
+      picture: "",
+      description: "",
+      price: "",
+      email: "",
+      phone: "",
+    })
 
     superagent
       .post('https://second-handshop.herokuapp.com/advertisements')
       .send(newAd)
       .then( res => console.log("Submitted !!", res) )
       .catch(console.error)
+
+      this.setState({submitted: true})
   }
 
   render() {
     return (
-      <div>
-        <h1>Publish Your Advertisement</h1>
-        {this.state.title && this.state.title}
+      <div style={{'margin-top': '100px', 'text-align': 'center'}}>
+        <h2>Publish Your Advertisement</h2>
+        {this.state.submitted && <h2>Submitted!</h2>}
         <form onSubmit={this.handleSubmit}>
-          <div className="card-wrapper">
+          <div style={{'margin-top': '50px'}}>
             <div className="single-card">
+            <h2>Your Ad</h2>
 
-            <h2>Set an image for your product: </h2>
+            <p>Image:
             <input 
               type="url" 
               id="url"
               placeholder="https://example.com"
               pattern="https://.*" size="30"
-              onChange={this.handleChange} name="picture"
+              onChange={this.handleChange} 
+              name="picture"
+              value={this.state.picture}
             />
+            </p>
 
-            <h1>Title: <input type="text" onChange={this.handleChange} name="title"/></h1>
+            Title: 
+              <input 
+                type="text" 
+                onChange={this.handleChange} 
+                name="title"
+                value={this.state.title}
+              />
+            
 
             <p className="price">Price: 
               <input type="number" min="1" step="any"
-              onChange={this.handleChange} name="price" />
+              onChange={this.handleChange}
+              value={this.state.price} 
+              name="price" />
             </p>
             <p>Description: 
               <input 
                 type="textarea" 
-                onChange={this.handleChange} name="description"
+                onChange={this.handleChange} 
+                value={this.state.description}
+                name="description"
               /></p>
 
             <p>Email: 
               <input 
                 type="email" 
-                onChange={this.handleChange} name="email"
+                onChange={this.handleChange}
+                value={this.state.email} 
+                name="email"
               /></p>
             <p>Phone: 
               <input 
                 type="phone" 
-                onChange={this.handleChange} name="phone"
+                onChange={this.handleChange}
+                value={this.state.phone} 
+                name="phone"
                 /></p>
             <p>
               <button onClick={this.handleSubmit} >Add your Add</button></p>
